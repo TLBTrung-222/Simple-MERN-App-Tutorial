c06355c1337b89d812d0dd0444b44192fedc0078
Trung Trần <trungtran@MacBook-Air-cua-Trung.local>
2023-06-14 22:21:18 +0700

    Add database connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@
 const express = require('express')
 //* Creates an Express application. The express() function is a top-level function exported by the express module.
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://user123:password123Tech@cluster0.4ukcz5f.mongodb.net/MernTutorial?retryWrites=true&w=majority')
+
 
 //* add listen port for backend (3001 because React use 3000)
 app.listen(3001, ()=> {
