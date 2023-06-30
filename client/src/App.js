07f9eb269f04531529a5cef549d8dc557e7652a9
Trung Trần <trungtran@MacBook-Air-cua-Trung.local>
2023-06-30 17:35:14 +0700

    Create post request from frontend

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,10 @@ import Axios from 'axios';
 
 function App() {
   const [listOfUsers, setListOfUsers] = useState([])
+  const [name, setName] = useState("")
+  const [age, setAge] = useState(0)
+  const [username, setUsername] = useState("")
+
 
   //* display list of user by default
   useEffect(() => {
@@ -12,6 +16,12 @@ function App() {
       setListOfUsers(response.data)
     })
   }, [])
+
+  const createUser = () => {
+    Axios.post("http://localhost:3001/createUser", { name: name, age: age, username: username }).then((response) => {
+      setListOfUsers([...listOfUsers, { name: name, age: age, username: username }])
+    })
+  }
 
   return (
     <div className="App" >
@@ -24,6 +34,19 @@ function App() {
             <h1>Username: {user.username}</h1>
           </div>
         })}
+      </div>
+
+      <div>
+        <input type="text" placeholder='Name...' onChange={(event) => {
+          setName(event.target.value)
+        }} />
+        <input type="number" placeholder='Age...' onChange={(event) => {
+          setAge(event.target.value)
+        }} />
+        <input type="text" placeholder='Username...' onChange={(event) => {
+          setUsername(event.target.value)
+        }} />
+        <button onClick={createUser}>Create User</button>
       </div>
     </div>
   );
